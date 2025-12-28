53b590a90f3d4bf8a4446f94b4ee24db9200c540
dipghoshraj <dipghoshraj@gmail.com>
2025-12-29 00:54:07 +0530

    build the go routine hasher

--- goengine/hasher.go
+++ goengine/hasher.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"crypto/sha256"
 	"goengine/engine"
 	"goengine/simple"
+	"sync"
 
 	flatbuffers "github.com/google/flatbuffers/go"
 )
+
+const MaxWorkers = 64
+
+type job struct {
+	index    int
+	userData string
+}
 
 func handleHashRequest(req *engine.Request) ([]byte, error) {
 
@@ -15,16 +24,50 @@ func handleHashRequest(req *engine.Request) ([]byte, error) {
 
 	inputs := make([]string, datalen)
 
-	for i := 0; i < datalen; i++ {
+	for i := range datalen {
 		inputs[i] = string(in.Data(i))
 	}
+
+	type hashResult struct {
+		userdata string
+		hash     string
+	}
+
+	workerCount := min(len(inputs), MaxWorkers)
+
+	results := make([]hashResult, len(inputs))
+	jobs := make(chan job)
+
+	wg := sync.WaitGroup{}
+
+	for range workerCount {
+		wg.Go(func() {
+			for j := range jobs {
+				hashed := buildHash(j.userData)
+				results[j.index] = hashResult{
+					userdata: j.userData,
+					hash:     hashed,
+				}
+			}
+		})
+	}
+
+	for i, v := range inputs {
+		jobs <- job{
+			index:    i,
+			userData: v,
+		}
+	}
+
+	close(jobs)
+	wg.Wait()
 
 	b := flatbuffers.NewBuilder(1024)
 	offsets := make([]flatbuffers.UOffsetT, len(inputs))
 
-	for i, v := range inputs {
-		user := b.CreateString(v)
-		hash := b.CreateString("hash_" + v)
+	for i, v := range results {
+		user := b.CreateString(v.userdata)
+		hash := b.CreateString(v.hash)
 		simple.HashResponseStart(b)
 		simple.HashResponseAddUserData(b, user)
 		simple.HashResponseAddHashData(b, hash)
@@ -45,4 +88,10 @@ func handleHashRequest(req *engine.Request) ([]byte, error) {
 	b.Finish(hashOut)
 
 	return b.FinishedBytes(), nil
+}
+
+func buildHash(data string) string {
+	// Dummy hash function for illustration
+	hashdata := sha256.Sum256([]byte(data))
+	return string(hashdata[:])
 }
